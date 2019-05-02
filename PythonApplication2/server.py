5cdeb832164abd54aca2b84ca2066999b49c90c5
jetpeng <cs1002520@gmail.com>
2019-05-02 17:15:13 +0800

    ok

---
+++ PythonApplication2/server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+sys.path.append('./gen-py')
+
+from helloworld import HelloWorld
+from helloworld.ttypes import *
+
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+class HelloWorldHandler:
+  def ping(self):
+    return "pong"
+
+  def say(self, msg):
+    ret = "Received: " + msg
+    print ret
+    return ret
+
+handler = HelloWorldHandler()
+processor = HelloWorld.Processor(handler)
+transport = TSocket.TServerSocket("localhost", 9090)
+tfactory = TTransport.TBufferedTransportFactory()
+pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+
+print "Starting thrift server in python..."
+server.serve()
+print "done!"
