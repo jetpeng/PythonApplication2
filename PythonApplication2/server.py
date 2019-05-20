70e08c5df9ecf4b0b860b246090dc98887b765ae
peng yiting <cs1002520@gmail.com>
2019-05-20 14:02:20 +0800

    Revert "111"
    This reverts commit de48670bf03ccc048263249e4e7cc5846f49d871.

--- PythonApplication2/server.py
+++
@@ -1,9 +0,0 @@
-from xmlrpc.server import SimpleXMLRPCServer
-
-def speak_your_words(s):
-    print("message from client:", s)
-    return "message from server: " + s
-
-server = SimpleXMLRPCServer(("localhost",9999)) # 绑定端口
-server.register_function(speak_your_words) # 注册函数
-server.serve_forever() #启动监听
